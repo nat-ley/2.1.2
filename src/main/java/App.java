007b357675da3288a40c4a6a80654dbbaf740432
nat-ley <nat_ley@mail.ru>
2022-02-08 17:48:35 +0400

    Добавила класс Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean(HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+        System.out.println(bean == bean2);
+
+        Cat beanCat = applicationContext.getBean(Cat.class);
+        Cat bean2Cat = applicationContext.getBean(Cat.class);
+        System.out.println(beanCat == bean2Cat);
+
     }
 }
